87048256aee9e97d448f466f91843c036695dc33
brown.h <brown.h@kakaobank.com>
2018-08-08 18:26:02 +0900

    add labels

--- prom_pg_client/push.py
+++ prom_pg_client/push.py
@@ -1,14 +1,15 @@
 from prometheus_client import CollectorRegistry, Gauge, push_to_gateway
 
-def push_to_gateway(name, temperature, humidity, job='romaine'):
+registry = CollectorRegistry()
+gauge_temperature = Gauge('temperature', 'Current temperature in Celsius', ['location'],
+                              registry=registry)
+gauge_humidity = Gauge('humidity', 'Current humidity level in %', ['location'],
+                    registry=registry)
+
+def to_gateway(location, temperature, humidity, job='romaine'):
     push_gw_svr_path = "pushgateway.romaine.ly.lv"
 
-    registry = CollectorRegistry()
-
-    gauge_temperature = Gauge('_'.join(name, 'temperature'), 'Current temperature in Celsius', registry=registry)
-    gauge_temperature.set(temperature)
-
-    gauge_humidity = Gauge('_'.join(name, 'humidity'), 'Current humidity level in %', registry=registry)
-    gauge_humidity.set(humidity)
+    gauge_temperature.labels(location).set(temperature)
+    gauge_humidity.labels(location).set(humidity)
 
     push_to_gateway(push_gw_svr_path, job=job, registry=registry)
