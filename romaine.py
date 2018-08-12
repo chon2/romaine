a0fa957ec5a7c2eb4858c1fa7520e778b4b3e7d2
kidew <chon2@hanmail.net>
2018-08-12 10:49:32 +0000

    telegram noti

--- romaine.py
+++ romaine.py
@@ -1,12 +1,28 @@
 import sensor
 from prom_pg_client import push
 import time
+import telegram
+import requests
 
 # Intervals of about 2 seconds or less will eventually hang the DHT22.
 INTERVAL = 60
 DHT22_PIN_NUM = 4
 
+telegram_bot_token = "616109347:AAGBMYhjA9GY_Oehd--3lo6bTdqkrgFSwqU"
+chat_id = 476794024
+
+def get_ngrok_status():
+    resp = requests.get("http://localhost:4040/api/tunnels")
+    return resp.json()
+
+def send_telegram(message, chat_id=chat_id):
+    bot = telegram.Bot(token=telegram_bot_token)
+    bot.send_message(chat_id, message)
+
 if __name__ == '__main__':
+    ngrok_status = get_ngrok_status()
+    send_telegram(ngrok_status)
+
     pi = sensor.start_pigpio()
     dht22 = sensor.start_dht22(pi, DHT22_PIN_NUM)
     sensor.read_dht22(dht22)
--- romaine.sh
+++ romaine.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 
+cd $SCRIPT_DIR
+
+. ./venv/bin/activate
+
 echo "start ngrok"
 
-ngrok start ssh --log=stdout --config=$SCRIPT_DIR/ngrok.yml
+ngrok start ssh --log=stdout --config=$SCRIPT_DIR/ngrok.yml &
 
-#sudo pigpiod
-#
-#cd $SCRIPT_DIR
-#python romaine.py &
+sudo pigpiod
+
+python romaine.py &
