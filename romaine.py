0efb787cff3744c1d52fdad1933c5fdc960b50d1
kidew <chon2@hanmail.net>
2018-08-17 13:14:26 +0000

    sentry

--- romaine.py
+++ romaine.py
@@ -3,6 +3,11 @@ from prom_pg_client import push
 import time
 import telegram
 import requests
+import traceback
+
+from raven import Client
+
+client = Client('https://063ad6fa9d854929968e523ad81d0db2:c3c6144b11ed449584aedcd4a802c499@sentry.io/1264249')
 
 # Intervals of about 2 seconds or less will eventually hang the DHT22.
 INTERVAL = 60
@@ -21,7 +26,11 @@ def send_telegram(message, chat_id=chat_id):
 
 if __name__ == '__main__':
     ngrok_status = get_ngrok_status()
-    send_telegram(ngrok_status)
+
+    try:
+        send_telegram(ngrok_status)
+    except Exception:
+        traceback.print_exc()
 
     pi = sensor.start_pigpio()
     dht22 = sensor.start_dht22(pi, DHT22_PIN_NUM)
@@ -35,11 +44,15 @@ if __name__ == '__main__':
             temperature = dht22.temperature()
             print("t:{}, h:{}%".format(temperature, humidity))
             push.to_gateway('area 1', temperature, humidity, job='romaine')
+        except KeyboardInterrupt :
+            break 
+        except Exception :
+            traceback.print_exc()
+        finally:
             dht22.sensor_resets()
             next_reading += INTERVAL
             time.sleep(next_reading-time.time())
-        except KeyboardInterrupt :
-            break 
+           
     sensor.stop_dht22(dht22)
     sensor.stop_pigpio(pi)
     print("terminated this program...")
